cdd4e935ed8527a9afeb2086e45b8dc188d95826
Emilie Uthaiwat <emiliepathum@gmail.com>
2023-06-07 01:16:07 +0200

    Marc's proof

--- src/groupes/groupes.lean
+++ src/groupes/groupes.lean
@@ -8,5 +8,17 @@ variables (G : Type) [group G]
 peut être décomposé plus avec un autre `split`. -/
 example (n : ℕ) (hn : 2 ≤ n) : is_subgroup ({z | z ^ n = 1} : set ℂˣ) :=
 begin
-  sorry
+  split,
+    {
+        split,
+        {simp},
+        {intros a b aU bU,
+          simp at *,
+          rw [mul_pow, aU, bU],
+          norm_num,}
+    },
+    {intros a aU,
+      simp,
+      exact aU,
+   },
 end
