7cc07825063dd4a3e9b73818b3c8a1de99e8546b
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-06-14 17:45:45 +0200

    COntinue with the is_eigenvecctor_implies_is_root proof

--- src/diagonalisation/diagonalisation.lean
+++ src/diagonalisation/diagonalisation.lean
@@ -2,6 +2,7 @@ import linear_algebra.free_module.finite.basic
 import linear_algebra.charpoly.basic
 import linear_algebra.eigenspace.basic
 import linear_algebra.determinant
+import linear_algebra.matrix.charpoly.basic
 
 universes u v w
 
@@ -12,14 +13,15 @@ namespace End
 variables {R : Type u} {V : Type v} [field R]
 variables [add_comm_group V] [module R V] [module.finite R V] (f : End R V)
 
-
-lemma non_empty_ker_implies_det_zero   (h : f.ker ≠ ⊥) : 
--- I woudl want to have something like this:
--- f.det = 0 :=
--- but i cannot due to the hypothesis on f (End R M)
-f.det = 0 :=
+lemma non_empty_ker_implies_det_zero  {g: End R V} (h : g.ker ≠ ⊥) :
+  g.det = 0 :=
 begin
-  sorry,
+  by_contra' h0,
+  let g2 := linear_map.equiv_of_det_ne_zero g h0, --this is the same as `f`, but Lean knows it is bijective
+  apply h,
+  rw linear_map.ker_eq_bot,
+  intros x y hxy,
+  exact g2.injective hxy,
 end
 
 
@@ -28,7 +30,17 @@ theorem is_eigenvector_implies_is_root{μ : R} (h : f.has_eigenvalue μ) :
 begin
   rcases (submodule.ne_bot_iff _).1 h with ⟨v, ⟨H, ne0⟩⟩, -- (1)
   rw eigenspace at H, -- (2)
--- Objectif:
+  have H1 : (f - (algebra_map R (End R V)) μ).ker ≠ ⊥, -- (3)
+  {
+    -- This is trivial, I will do it latter. (It's just proving that a set is not empty having an element of the set...)
+    sorry,
+  },
+  have H2 := non_empty_ker_implies_det_zero H1, -- (4)
+  rw polynomial.is_root,
+  rw ← H2,
+  -- I'm stuck here. The goal is literally `det(f - μ • id) = det(f - μ • id)`, but Lean doesn't know that...
+
+-- Objective:
 -- (1) v ∈ eigenspace(μ)
 -- (2) v ∈ ker(f - μ • id)
 -- (3) ker(f - μ • Id) ≠ ∅
