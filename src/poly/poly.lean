0eee784e580f73f9b8b359c25cd7c4c39f22eeea
Biface <111498451+dresseurdechameau@users.noreply.github.com>
2023-06-23 14:03:41 +0200

    Update poly.lean

--- src/poly/poly.lean
+++ src/poly/poly.lean
@@ -11,7 +11,36 @@ variables (R : Type) [comm_ring R] (P : R[X])
 example (P ∉ non_zero_divisors R[X]) :
   ∃ (r : R), r ≠ 0 ∧ (C r) * P = 0 :=
 begin
-  sorry
+  induction h : P.nat_degree using nat.strong_induction_on with k hind generalizing P,
+  
+  dsimp at hind,
+  
+  by_cases hdeg : P.nat_degree = 0,
+  {
+    
+    have INT_1 : ∃ (α : R), ¬ α = 0 ∧ ((C α)*P).nat_degree = k-1,
+    {
+      sorry
+    },
+    
+    obtain ⟨α,b,ha⟩ := INT_1,  
+
+    have INT_2 : ∃ (β : R), ¬ β = 0 ∧ ((C β)*((C α)*P)) = 0,   
+    {
+      sorry
+    },
+
+    obtain ⟨β,b,hb⟩ := INT_2,
+
+    -- appliquer l'associativité et normalement c'est gagné
+    -- mais problème pour montrer que alphabeta != 0
+
+  },
+  {
+    --P.erase_lead
+    sorry
+  }
+
 end
 
 example (H : is_nilpotent P) : ∀ i, is_nilpotent (P.coeff i) :=
